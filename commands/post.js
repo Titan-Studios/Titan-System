0900c4be0c53bfe152e1b445b605f57761cd65d7
DSTitan <72593433+DSTitan@users.noreply.github.com>
2020-11-29 01:54:33 -0400

    Titan System

---
+++ commands/post.js
@@ -0,0 +1,18 @@
+
+const talkedRecently = new Set();
+
+
+module.exports = {
+    commands: 'say',
+    minArgs: 1,
+    expectedArgs: '<message>',
+    permissionError: 'You cant use that command!',
+    callback: (message, arguments, text) => {
+
+        const text1 = arguments[1]
+  
+  
+
+    },
+    permissions: [],
+}
