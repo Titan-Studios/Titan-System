a572daa291cd50c5628c5b5f773d547ccb044814
DSTitan <72593433+DSTitan@users.noreply.github.com>
2021-08-31 14:07:27 -0400

    Update 8/31/21

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ client.on('ready', () => {
     Modules.userJoinAndLeave(client);
     Modules.distubeMusic(distubeClient);
     Modules.button(client);
+    Modules.repeater();
 
     // Client status
     client.user.setActivity({
